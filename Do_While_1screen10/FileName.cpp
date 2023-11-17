517f423de0a232a5e88a678fdec7ae3ad0012b27
baris <baris@BARISSOMEROGLU>
2023-11-18 00:22:17 +0300

    Add project files.

---
+++ Do_While_1screen10/FileName.cpp
@@ -0,0 +1,30 @@
+/*
+
+We will write numbers on the screen from 1 to 10.
+
+Coder: Bar?? Somero?lu
+Date: 18.11.23 / 00.20 a.m.
+
+*/
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int i = 1;
+
+	do
+	{
+		cout << i << endl;
+
+		i++;
+
+	} while (i <= 10);
+
+
+	return 0;
+}
